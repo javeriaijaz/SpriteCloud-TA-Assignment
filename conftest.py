1cb6447c10a1e6c43ea4ec0650d8692fe8dedf75
Javeria <Javeria@Javerias-Mac.local>
2025-02-26 23:00:55 +0500

    Initial commit - Set up project

--- conftest.py
+++ conftest.py
@@ -1,6 +1,9 @@
 import pytest
+import ssl
 from selenium import webdriver
 import chromedriver_autoinstaller
+
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # Automatically install the correct version of ChromeDriver
 chromedriver_autoinstaller.install()
@@ -12,13 +15,13 @@ def setup():
     chrome_options.add_argument("--disable-extensions")  # Disable extensions to ensure clean test runs
     chrome_options.add_argument("--no-sandbox")  # Avoid sandboxing issues, common in CI environments
     chrome_options.add_argument("--disable-dev-shm-usage")  # Avoid issues related to limited memory in CI environments
-    chrome_options.add_argument("--headless")  # Run Chrome in headless mode (no UI)
+    #chrome_options.add_argument("--headless")  # Run Chrome in headless mode (no UI)
     chrome_options.add_argument("--remote-debugging-port=9222")  # Use remote debugging port (for troubleshooting)
     chrome_options.add_argument("--verbose")  # Enable verbose logging
 
     # Initialize the WebDriver
     driver = webdriver.Chrome(options=chrome_options)
-
+    driver.maximize_window()
     yield driver  # Yield the driver to the test
 
     driver.quit()  # Quit the WebDriver after the test is done
