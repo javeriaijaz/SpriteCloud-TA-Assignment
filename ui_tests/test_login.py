7dd10854a243b3a35d900e3a6ce1009d38118ebf
Javeria <javeriaijaz19@gmail.com>
2025-02-27 20:28:37 +0500

    Final commits

--- ui_tests/test_login.py
+++ ui_tests/test_login.py
@@ -1,5 +1,8 @@
 import pytest
 from pages.login_page import LoginPage
+from faker import Faker
+
+fake = Faker()
 
 @pytest.mark.usefixtures("setup")
 class TestLogin:
@@ -13,7 +16,7 @@ class TestLogin:
         ],
         expected="The error message should contain 'Epic sadface', indicating login failure."
     )
-    @pytest.mark.parametrize("username, password", [("wrong_user", "wrong_pass")])
+    @pytest.mark.parametrize("username, password", [(fake.user_name(), fake.password())])
     def test_invalid_login(self, setup, username, password):
         """
         Test Case: Verify that an invalid login attempt shows the correct error message.
@@ -21,7 +24,7 @@ class TestLogin:
         driver = setup  # Assign the WebDriver instance from the pytest fixture
         login_page = LoginPage(driver)  # Initialize the LoginPage with WebDriver
 
-        login_page.open()  # Open the SauceDemo login page
+        login_page.open()  # Open the login page
         login_page.login(username, password)  # Enter invalid credentials
 
         # Validate that the correct error message is displayed
